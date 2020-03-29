2a234d3efaf629175e4caf117f9c29013c1640a0
evantilton <evantilton@gmail.com>
2020-03-29 09:06:59 -0500

    added the p5 library and got it to draw the canvas

--- trees.js
+++ trees.js
@@ -1,10 +1,15 @@
-const canvas = document.getElementById('game');
-const context = canvas.getContext('2d');
+// const canvas = document.getElementById('game');
+// const context = canvas.getContext('2d');
 
+// context.scale(20, 20);
+// // function setup() {
+// //     createCanvas(400, 400)
+// // }
 function setup() {
-    createCanvas(400, 400)
+    createCanvas(400, 400);
 }
 
 function draw() {
     background(51);
 }
+

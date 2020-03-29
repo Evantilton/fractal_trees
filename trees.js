61d55588fc8710ce70f425df5d42ec316350ee21
evantilton <evantilton@gmail.com>
2020-03-29 09:23:18 -0500

    got it to draw a second line at 45 degrees

--- trees.js
+++ trees.js
@@ -8,9 +8,15 @@ function draw() {
     let length = 100;
     //this sets the line to white, 255 is white
     stroke(255);
-    //line draws a line, 200 is halfways
+    //translate sets where I am starting from, it sets the start location
+    translate(200, height);
     branch(100);
 }
 function branch(length) {
-    line(200, height, 200, height-length)
+    line(0, 0, 0, -length);
+    //I now want to draw from the new location at the top of the first line
+    translate(0, -length)
+    //pi / 4 is 45 degrees
+    rotate(PI/4);
+    line(0, 0, 0, -length);
 }

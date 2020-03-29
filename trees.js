3cd3c7b0b5d0d690fbbbe854693ed9fd94334ba5
evantilton <evantilton@gmail.com>
2020-03-29 08:55:49 -0500

    first commit

---
+++ trees.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+
+function setup() {
+    createCanvas(400, 400)
+}
+
+function draw() {
+    background(51);
+}

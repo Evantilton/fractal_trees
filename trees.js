e61c51f24fc73670e27dece8a4dff960561a0c06
evantilton <evantilton@gmail.com>
2020-03-29 09:46:05 -0500

    added the second side, using the p5 push and pop to retrieve past locations

--- trees.js
+++ trees.js
@@ -1,10 +1,13 @@
-
+let angle = 0;
+let slider;
 function setup() {
     createCanvas(400, 400);
+    slider = createSlider(0,TWO_PI, PI / 4);
 }
 
 function draw() {
     background(51);
+    angle = slider.value();
     let length = 100;
     //this sets the line to white, 255 is white
     stroke(255);
@@ -17,7 +20,14 @@ function branch(length) {
     //I now want to draw from the new location at the top of the first line 
     translate(0, -length)
     //pi / 4 is 45 degrees
-    rotate(PI/4);
-    if (length > 2);
-    branch(length * 0.67)
+    rotate(angle);
+    if (length > 4) {
+    push();
+    branch(length * 0.67);
+    pop();
+    push();
+    rotate(-angle);
+    branch(length * 0.67);
+    pop();
+    };
 }

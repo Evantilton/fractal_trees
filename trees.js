e2e3f2052eceb4d15fb396b21c8737ad53d13d0c
evantilton <evantilton@gmail.com>
2020-03-29 09:11:11 -0500

    got it to add a white line

--- trees.js
+++ trees.js
@@ -1,15 +1,14 @@
-// const canvas = document.getElementById('game');
-// const context = canvas.getContext('2d');
 
-// context.scale(20, 20);
-// // function setup() {
-// //     createCanvas(400, 400)
-// // }
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
     background(51);
+    let length = 100;
+    //this sets the line to white, 255 is white
+    stroke(255);
+    //line draws a line, 200 is halfways
+    line(200, height, 200, height-length);
 }
 
